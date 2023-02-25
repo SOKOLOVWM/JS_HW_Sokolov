d01fc32a8b0a752c409fe002cddd1da9843089a1
Vladimir <sokolovwm@mail.ru>
2023-02-25 22:05:43 +0300

    add: solution task 06

--- script.js
+++ script.js
@@ -59,3 +59,12 @@ console.log(`Объем цилиндра: ${VCilindra}м`);
 const radius = 5;
 const SKruga = +(Math.PI * Math.pow(radius, 2)).toFixed(2);
 console.log(`Площадь круга ${SKruga}см`);
+
+//Task 06
+
+let a = 5;
+let b = 7;
+let h = 10;
+const STrap = ((a + b) / 2) * h;
+console.log(`Площадь трапеции ${STrap}см`);
+console.log(typeof STrap);
