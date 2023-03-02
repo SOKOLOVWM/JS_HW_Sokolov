1c5984289ad5b1337384ffc38fc3b4f64e3d84c7
Vladimir <sokolovwm@mail.ru>
2023-03-02 10:36:03 +0300

    add: solution task 02

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 "use strict";
 
-/* Создайте переменные: name, age, city, phone, email, company; и получите через ф-цию
-prompt() соответствующие значения: ваше имя, возраст, город проживания и т.д.
-Выведите на экран фразу: «Меня зовут %Имя%. Мне %Возраст% лет. Я проживаю в
-городе %Город% и работаю в компании %Компания%. Мои контактные данные:
-%Телефон%, %Почта%.». */
+/* COMMON VARIABLES*/
+
+// const name = prompt("Your name");
+// const age = prompt("Your age");
 
 /* TASK 01 */
 
-{
-	const name = prompt("Your name");
-	const age = prompt("Your age");
-	const city = prompt("Your city");
-	const phone = prompt("Your phone");
-	const email = prompt("Your email");
-	const company = prompt("Your company");
+// {
+// 	const city = prompt("Your city");
+// 	const phone = prompt("Your phone");
+// 	const email = prompt("Your email");
+// 	const company = prompt("Your company");
 
-	console.log(`
-		My name is ${name}.
-		I am ${age} years old.
-		I live in ${city}.
-		My phone is ${phone}.
-		My e-mail is ${email}.
-		I work for ${company}.
-	`);
+// 	console.log(`
+// 		My name is ${name}.
+// 		I am ${age} years old.
+// 		I live in ${city} and work for ${company}.
+// 		My contacts are: phone ${phone}, e-mail ${email}.
+// 	`);
+// }
+
+/* TASK 02 */
+
+{
+	const yearCurrant = 2023;
+	const yearBirth = yearCurrant - age;
+	console.log(`${name} was born in ${yearBirth}.`);
 }
